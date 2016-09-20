e4b5425751f32a528e4d35f98b6110ead8b390af
williamshty <williamshty@gmail.com>
2016-09-20 15:14:34 +0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Author: Shi Tianyuan
+
+*/
+include('includes/header_login.html');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+<style type="text/css">
+
+input.submit{background-color: #34495E;
+        color:#1ABC9C;
+        
+        margin-top: 20px;
+        margin-bottom: 40px;
+        width: 150px;
+        text-align: center;
+
+    }</style>
+</head>
+<body>
+<?php
+	require('db.php');
+    // If form submitted, insert values into the database.
+    if (isset($_POST['username'])){
+        $username = $_POST['username'];
+		$email = $_POST['email'];
+        $pass = $_POST['pass'];
+		$username = stripslashes($username);
+		$username = mysqli_real_escape_string($connection,$username);
+		$email = stripslashes($email);
+		$email = mysqli_real_escape_string($connection,$email);
+		$pass = stripslashes($pass);
+		$pass = mysqli_real_escape_string($connection,$pass);
+		
+        $query = "INSERT into `users` (username, pass, email) VALUES ('$username', '".md5($pass)."', '$email')";
+        $result = mysqli_query($connection,$query);
+        if($result){
+            echo "<div class='form'><h3>You are registered successfully.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
+?>
+<div class="form" align="center">
+<h3>Registration</h3>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required /><br>
+<input type="email" name="email" placeholder="Email" required /><br>
+<input type="password" name="pass" placeholder="Password" required /><br>
+<input class="submit" type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+</body>
+<?php
+include("includes/footer.html");
+?>
+</html>
